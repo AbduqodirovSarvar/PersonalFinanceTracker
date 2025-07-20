8c7fafecd41d7c9bc6f70c262b49ccf8ad37b1e8
Sarvar Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2025-07-21 02:42:20 +0500

    added redis

--- Infrastructure/DependencyInjection/DependencyInjection.cs
+++ Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -7,16 +7,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
 
 namespace Infrastructure.DependencyInjection
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddScoped<ICurrentUserService, CurrentUserService>();
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IHashService, HashService>();
+
+            var redisConnection = configuration.GetConnectionString("Redis");
+            services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(redisConnection!));
+            services.AddScoped<IRedisCacheService, RedisCacheService>();
+
 
             return services;
         }
---
+++ Infrastructure/Services/RedisCacheService.cs
@@ -0,0 +1,35 @@
+﻿using Application.Interfaces;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class RedisCacheService(IConnectionMultiplexer redis) : IRedisCacheService
+    {
+        private readonly IDatabase _db = redis.GetDatabase();
+
+        public async Task SetAsync<T>(string key, T value, TimeSpan? expiry = null)
+        {
+            var json = JsonSerializer.Serialize(value);
+            await _db.StringSetAsync(key, json, expiry);
+        }
+
+        public async Task<T?> GetAsync<T>(string key)
+        {
+            var value = await _db.StringGetAsync(key);
+            return value.HasValue
+                ? JsonSerializer.Deserialize<T>(value!)
+                : default;
+        }
+
+        public async Task RemoveAsync(string key)
+        {
+            await _db.KeyDeleteAsync(key);
+        }
+    }
+}
